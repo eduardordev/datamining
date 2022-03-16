0e9fa9d7279aff173cf90f6f4e6bb8310a726734
RAMIREZ HERRERA <ram19946@uvg.edu.gt>
2022-03-16 15:53:44 -0600

    update

--- HT4/ht4.R
+++ HT4/ht4.R
@@ -1,133 +1,92 @@
-library(rpart)
-library(caret)
-library(tree)
 library(rpart.plot)
-library(randomForest)
+library(ModelMetrics)
 library(ggplot2)
-library(ggpubr)
+library(rpart)
+library(randomForest)
 
-library(cluster) 
-library(e1071)
-library(mclust) 
-library(fpc) 
-library(NbClust) 
-library(factoextra)
-
-datosTrain <- read.csv("train.csv")
-theme_set(theme_pubr())
-# Ejercicio 3 al 11
-
-
-wss <- (nrow(datosTrain[,c(1,2,44:53,81)])-1)*sum(apply(datosTrain[,c(1,2,44:53,81)],2,var))
-
-for (i in 2:10) 
-  wss[i] <- sum(kmeans(datosTrain[,c(1,2,44:53,81)], centers=i)$withinss)
-
-plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")# Grafica para concluir la cantidad de clusters
-
-#se concluye que son 4 clusters, el metodo a utilizar es k means
-
-conjunto<-datosTrain
-datosCompleto<-datosTrain[complete.cases(conjunto),]
-km<-kmeans(datosTrain[,c(1,2,44:53,81)],4,iter.max =100)
-conjunto$grupo<-km$cluster
-
-g1<- conjunto[conjunto$grupo==1,]
-prop.table(table(g1$SalePrice))*100
-nrow(g1)
-summary(g1)
-
-g2<- conjunto[conjunto$grupo==2,]
-prop.table(table(g2$SalePrice))*100
-g3<- conjunto[conjunto$grupo==3,]
-prop.table(table(g3$SalePrice))*100
-g4<- conjunto[conjunto$grupo==4,]
-prop.table(table(g4$SalePrice))*100
-
-plotcluster(datosTrain[,c(1,2,44:53,81)],km$cluster) #grafica de k means
-
-
-
-
-#resumen de cada grupo del cluster
-summary(g1)
-summary(g2)
-summary(g3)
-summary(g4)
-
-# resumen del dataset
-summary(datosTrain$SalePrice)
-
-#arboles de decision
-theme_set(theme_pubr())
-porciento <- 70/100
-
+#ejercicio2
+percent<-70/100
 set.seed(123)
+datos<-read.csv('train.csv')
+percentil <- quantile(datos$SalePrice)
+View(datos)
 
-trainRowsNumber<-sample(1:nrow(datosTrain),porciento*nrow(datosTrain))
-train<-datosTrain[trainRowsNumber,]
-test<-datosTrain[-trainRowsNumber,]
-arbolModeloRegresion<-rpart(SalePrice~.,train,method = "anova")
-rpart.plot(arbolModeloRegresion)
-#Se crea el test y se clasifica
-head(test)
-#Se crea la variable respuesta en base a la clasificacion hipotetica
-datosTrain$Clasificacion <- ifelse(datosTrain$SalePrice <=152000, "Economica",
-                                   ifelse(datosTrain$SalePrice <=229000, "Intermedia", "Cara"))
+estado<-c('Estado')
+datos$Estado<-estado
+
+datos <- within(datos, Estado[SalePrice<=129975] <- 0)
+datos$Estado[(datos$SalePrice>129975 & datos$SalePrice<=163000)] <- 1
+datos$Estado[datos$SalePrice>163000] <- 2
+corte <- sample(nrow(datos),nrow(datos)*percent)
+
+#datos entrenados
+train<-datos[corte,]
+test<-datos[-corte,]
+
+#Regresion lineal de los datos
+fitLMPW<-lm(SalePrice~ ., data = train[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd", "SalePrice","LotArea")])
+predL<-predict(fitLMPW, newdata = test)
+
+#prediccion
+resultados<-data.frame(test$SalePrice,predL)
+ej2<-head(resultados, n=5)
+graf1<-ggplot(datos=train,mapping = aes(x=SalePrice,y=GrLivArea ))+
+  geom_point(color='red',size=2)+
+  geom_smooth(method = 'lm',se=TRUE,color='black')+
+  labs(title = 'Precio de venta ~ Pies cuadrados de vivienda',x="Precio de venta",y='Pies cuadrados de venta')+
+  theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+
+#ejercicio3
+pairs(datos$SalePrice ~ datos$GrLivArea)
+pairs(datos$SalePrice ~ datos$YearBuilt)
+pairs(datos$SalePrice ~ datos$BsmtUnfSF)
+pairs(datos$SalePrice ~ datos$TotalBsmtSF)
+pairs(datos$SalePrice ~ datos$GarageArea)
+pairs(datos$SalePrice ~ datos$YearRemodAdd)
+pairs(datos$SalePrice ~ datos$LotArea)
+
+ggplot(datos=train,mapping = aes(x=SalePrice,y=GrLivArea ))+
+  geom_point(color='red',size=2)+
+  geom_smooth(method = 'lm',se=TRUE,color='black')+
+  labs(title = 'Precio de venta ~ Pies cuadrados de vivienda',x="Precio de venta",y='Pies cuadrados de venta')+
+  theme_bw()+theme(plot.title = element_text(hjust = 0.5))
+
+predL<-predict(fitLMPW, newdatos = test)
+
+summary(predL)
+summary(test$SalePrice)
+
+plot(fitLMPW)
+
+boxplot(fitLMPW$residuals)
+
+fitLMPW2 <- replace_outliers(fitLMPW$residuals)
+par(mfrow = c(1,2))
+
+boxplot(fitLMPW2, main = "Sin datos atipicos",col=6)
+hist(fitLMPW2)
 
 
+plot(test$SalePrice, test$LotArea)
+points(predL, test$LotArea, col="red",pch=15)
 
-trainRowsNumber<-sample(1:nrow(datosTrain),porciento*nrow(datosTrain))
-#conjuntos de entramiento y prueba
-train<-datosTrain[trainRowsNumber,]
-test<-datosTrain[-trainRowsNumber,]
-arbolModeloClasificacion<-rpart(Clasificacion~.,train,method = "class")
-#se muestra graficamente el modelo de clasificacion
-rpart.plot(arbolModeloClasificacion)
-
-save(train,test,arbolModeloClasificacion, file = "Variables.RData")
-load("Variables.RData")
-train2<- data.frame(train$SalePrice,train$Clasificacion)
-colnames(train2)[2]<-"Clasificacion"
-colnames(train2)[1]<-"SalePrice"
-dt_model<-rpart(Clasificacion~.,train2,method = "class")
-plot(dt_model);text(dt_model)
-prp(dt_model)
-rpart.plot(dt_model)
-#Se crea el test y se clasifica
-head(test)
-aa <- data.frame(test$SalePrice)
-colnames(aa)[1]<-"SalePrice"
-aa2 <-  subset(test, select=-c(Condition1))
-
-prediccion <- predict(dt_model,aa, se.fit=FALSE)
+plot(test$SalePrice, test$LotArea)
+points(predL, test$LotArea, col="red",pch=15)
+ 
+cor(datos$SalePrice,datos[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd","LotArea")], method = "spearman")
 
 
-columnaMasAlta<-apply(prediccion, 1, function(x) colnames(prediccion)[which.max(x)])
+cor(datos$SalePrice,datos$YearBuilt, method = "spearman")
 
-test$prediccion<-columnaMasAlta 
+cor(datos$SalePrice,datos$GarageArea, method = "spearman")
 
-cfm<-confusionMatrix(as.factor(test$prediccion),as.factor(test$Clasificacion))
+### Comparacion con el arbol de regresion:
 
-#Forest
-library(haven)
-library(dplyr)
+datos<-read.csv('train.csv') 
+df<-datos[,c("LotFrontage","LotArea","GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","X1stFlrSF","GarageYrBlt","GarageArea","YearRemodAdd", "SalePrice")]
+m1<-rpart(SalePrice ~ ., datos = df, method = "anova")
 
-datosTrain[sapply(datosTrain, is.character)] <- lapply(datosTrain[sapply(datosTrain, is.character)], as.factor)
+rpart.plot(m1, type = 3, digits = 3, fallen.leaves = TRUE )
 
-porciento <- 70/100
-set.seed(123)
-trainRowsNumber<-sample(1:nrow(datosTrain),porciento*nrow(datosTrain))
-train<-datosTrain[trainRowsNumber,]
-test<-datosTrain[-trainRowsNumber,]
 
-train <- train[, colSums(is.na(train)) == 0]
-test <- test[, colSums(is.na(test)) == 0]
-modeloRF1<-randomForest(as.factor(Clasificacion)~., data=train)
-
-prediccionRF1<-predict(modeloRF1,newdata = test[,1:63])
-testCompleto<-test
-testCompleto$predRF<-prediccionRF1
-
-cfmRandomForest <- confusionMatrix(testCompleto$predRF, as.factor(testCompleto$Clasificacion))
 
